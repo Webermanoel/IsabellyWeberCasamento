2d775d299de9b6f94f932d5d3c258b2c8e55a4be
Webermanoel <webermanoel22@gmail.com>
2026-08-19 18:34:17 -0400

    Subindo atualização

--- confirmar.js
+++ confirmar.js
@@ -1,7 +1,21 @@
 const confirma = document.getElementById("confirmar");
 const cancela = document.getElementById("cancelar");
 
+let eventoAtual = "";
+let nome = "";
+
+
 confirma.addEventListener("click", function(){
-    let nome = prompt("Digite seu nome:") 
+    eventoAtual = "";
+    nome = prompt("Para confirmar, digite seu nome:"); 
 })
+
+cancela.addEventListener('click', function(){
+    eventoAtual = "";
+    nome = prompt("Para cancelar, digite seu nome:")
+})
+
+if(eventoAtual === confirma){
+    alert("Presença confirmada");
+}
 
